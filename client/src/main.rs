2710da42e04f92b33fd6b11977fb9c5e66e225be
Liam Thorell <liamthorell18@gmail.com>
2025-02-04 17:04:04 +0100

    Added better error handling and code cleanup done
    Co-authored-by: Lukas Viering <freeeranger@users.noreply.github.com>

--- client/src/main.rs
+++ client/src/main.rs
@@ -8,7 +8,7 @@ use rand::Rng;
 use reqwest::blocking::Client;
 use sha2::{Digest, Sha256};
 use shared::{EncFile, EncFileResponse};
-use std::path::PathBuf;
+use std::{path::PathBuf, process::exit};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -27,59 +27,79 @@ enum CliCommand {
 struct UploadArgs {
     #[arg(required = true)]
     file: PathBuf,
+    #[arg(required = true)]
+    password: String,
 }
 
 #[derive(Parser)]
 struct RetrieveArgs {
     #[arg(required = true)]
     id: u16,
+    #[arg(required = true)]
+    password: String,
+    /// Output file stored in the specified path.
+    #[arg(short, long)]
+    out: Option<PathBuf>,
 }
 
 fn main() {
     let cli = Cli::parse();
 
     match &cli.command {
-        CliCommand::Upload(upload_args) => {
-            println!("uploading file: {}", upload_args.file.display());
+        CliCommand::Upload(upload_args) => upload(upload_args),
+        CliCommand::Retrieve(retrieve_args) => retrieve(retrieve_args),
+    };
+}
 
-            let content = read_file(&upload_args.file);
+fn upload(upload_args: &UploadArgs) {
+    println!("Uploading file: {}", upload_args.file.display());
 
-            let key = derive_key("haileywelsch");
+    let content = read_file(&upload_args.file);
 
-            let enc = encrypt(&content, &key);
+    let key = derive_key(&upload_args.password);
 
-            let client = Client::new();
-            let res = client
-                .post("http://localhost:8000/file")
-                .json(&enc)
-                .send()
-                .unwrap();
+    let enc = encrypt(&content, &key);
 
-            println!("response: {:?}", res.text().unwrap());
-        }
-        CliCommand::Retrieve(retrieve_args) => {
-            println!("retrieving file: {}", retrieve_args.id);
+    let client = Client::new();
+    let res = client
+        .post("http://localhost:8000/file")
+        .json(&enc)
+        .send()
+        .unwrap();
 
-            let client = Client::new();
-            let res = client
-                .get(format!("http://localhost:8000/file/{}", retrieve_args.id))
-                .send()
-                .unwrap();
+    println!(
+        "File successfully uploaded with id: {:?}",
+        res.text().unwrap()
+    );
+}
 
-            let res: EncFileResponse = res.json().unwrap();
+fn retrieve(retrieve_args: &RetrieveArgs) {
+    println!("retrieving file: {}", retrieve_args.id);
 
-            match res {
-                EncFileResponse::Success(file) => {
-                    let key = derive_key("haileywelsch");
+    let client = Client::new();
+    let res = client
+        .get(format!("http://localhost:8000/file/{}", retrieve_args.id))
+        .send()
+        .unwrap();
 
-                    let dec = decrypt(&file, &key);
+    let res: EncFileResponse = res.json().unwrap();
+    match res {
+        EncFileResponse::Success(file) => {
+            let key = derive_key(&retrieve_args.password);
+            let dec = decrypt(&file, &key);
 
-                    println!("dec: {:?}", String::from_utf8(dec).unwrap());
+            match &retrieve_args.out {
+                Some(out) => {
+                    std::fs::write(out, dec).unwrap();
+                    println!("File successfully written to: {}", out.display());
                 }
-                EncFileResponse::Error { error } => {
-                    println!("error: {}", error);
+                None => {
+                    println!("File contents: {:?}", String::from_utf8(dec).unwrap());
                 }
             }
+        }
+        EncFileResponse::Error { error } => {
+            println!("Error: {}", error);
         }
     }
 }
@@ -88,7 +108,8 @@ fn read_file(file: &PathBuf) -> Vec<u8> {
     match std::fs::read(file) {
         Ok(content) => content,
         Err(err) => {
-            panic!("Error reading file: {}", err);
+            println!("Error reading file: {}", err);
+            exit(0);
         }
     }
 }
@@ -105,7 +126,7 @@ fn encrypt(data: &Vec<u8>, key: &[u8; 32]) -> EncFile {
     let mut nonce = [0u8; 12];
     rng.fill(&mut nonce);
 
-    let mut enc = aes_gcm::AesGcm::new(KeySize::KeySize256, key, &nonce, &[0]);
+    let mut enc = aes_gcm::AesGcm::new(KeySize::KeySize256, key, &nonce, &[]);
 
     let input = data.as_slice();
     let mut output = vec![0u8; input.len()];
@@ -123,10 +144,15 @@ fn encrypt(data: &Vec<u8>, key: &[u8; 32]) -> EncFile {
 }
 
 fn decrypt(file: &EncFile, key: &[u8; 32]) -> Vec<u8> {
-    let mut dec = aes_gcm::AesGcm::new(KeySize::KeySize256, key, &file.nonce, &[0]);
+    let mut dec = aes_gcm::AesGcm::new(KeySize::KeySize256, key, &file.nonce, &[]);
 
     let mut output = vec![0u8; file.data.len()];
     dec.decrypt(&file.data, &mut output, &file.tag);
+
+    if output.iter().all(|&x| x == 0) {
+        println!("Invalid password");
+        exit(0);
+    }
 
     output.to_vec()
 }
